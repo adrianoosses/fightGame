40e0e0260409648dbeb09b3d8dcb25c93070e810
aosses <adrianoosses9@gmail.com>
2020-10-15 21:13:15 +0200

    Estructurarlo y ponerle imagenes

--- js/main.js
+++ js/main.js
@@ -50,12 +50,20 @@ class Fighter{
 class Player{
     constructor(){
         this.luchador = [];
+        this.contador = 0;
     }
     setLuchador(luchador){
         this.luchador.push(luchador);
     }
+    aumentaContador(){
+        this.contador++;
+    }
     getLuchador(){
         return this.luchador;
+    }
+
+    getContador(){
+        return this.contador;
     }
 }
 // name, attack, speed, luck, defense
@@ -63,6 +71,8 @@ let tyler = new Fighter("Tyler Durden", 80, 80, 90, 70);
 let jack = new Fighter("Jack", 70, 75, 95, 65);
 let angelFace = new Fighter("Angel Face", 60, 95, 95, 60);
 let mechanic = new Fighter("The Mechanic", 90, 60, 70, 100);
+let fig5 = new Fighter("Fig5", 95, 50, 96, 80);
+let fig6 = new Fighter("Fig6", 75, 80, 75, 85);
 
 //let arrayFighters = [player1, player2];
 
@@ -75,6 +85,8 @@ containerBox2.innerHTML = `
     <button id="fighter2" onclick="selection(jack)">F2</button>
     <button id="fighter3" onclick="selection(angelFace)">F3</button>
     <button id="fighter4" onclick="selection(mechanic)">F4</button>
+    <button id="fighter5" onclick="selection(fig5)">F5</button>
+    <button id="fighter6" onclick="selection(fig6)">F6</button>
 `;
 
 
@@ -85,14 +97,15 @@ let id = 0;
 arrayFighters = [];
 arrayPlayers = [];
 nMaxPlayers = 2;
+nMaxFighters = 3;
 function selection(nombre){
     if(arrayPlayers.length < nMaxPlayers){
         arrayPlayers.push(new Player());
         console.log("Mete player");
         console.log("mete1:",arrayPlayers);
     }
-    if(arrayFighters == 2){
-        console.log("Ya has elegido dos personajes");
+    if(arrayFighters == 3){
+        console.log("Ya has elegido tres personajes");
     }else{
         arrayPlayers[currentPlayerId].setLuchador(nombre);
         if(currentPlayerId == nMaxPlayers-1){
@@ -121,21 +134,27 @@ containerBox3.innerHTML = `
                 FIGHT
         </button>
     <P>Player 1 -</p>
-    <p id="p1Luchador"></p>
+    <p id="p1f1"></p>
     <p>Player 2 - </p>
-    <p id="p2Luchador"></p>
+    <p id="p2f1"></p>
 `;
 
 function cargarPersonajes(){
     console.log("entra en la funcion");
-    let p1Box = document.querySelector("#p1Luchador");
-    p1Box.innerHTML = `<p>${arrayPlayers[0].getLuchador().name}</p>`;
-    let p2Box = document.querySelector("#p2Luchador");
-    p2Box.innerHTML = `<p>${arrayPlayers[1].getLuchador().name}</p>`;
+    let p1f1Box = document.querySelector("#p1f1");
+    let p2f1Box = document.querySelector("#p2f1");
+    p1f1Box.innerHTML = ``;
+    p2f1Box.innerHTML = ``;
+    for(let i = 0; i < 3; i++){
+        p1f1Box.innerHTML += `<p>${arrayPlayers[0].getLuchador()[i].name}</p>`;
+        p2f1Box.innerHTML += `<p>${arrayPlayers[1].getLuchador()[i].name}</p>`;
+    }
+    
 }
 
 containerBox4.innerHTML = `
 <h1>FIGHT!</h1>
+<h2>Combate 1</h2>
 <div id="player1">
     <div id="lifeP1">
         <p>Life of Player 1: </p>
@@ -156,12 +175,13 @@ containerBox4.innerHTML = `
 `;
 
 containerBox5.innerHTML = `
-    <h1>Ha ganado el Jugador 1</h1>
-`;
+    <h1>Ha ganado<h1> 
+    <h1 id="winner"></h1>
+    <p>Victorias del Jugador 1</p>
+    <p id="p1victories"></p>
+    <p">Victorias del Jugador 2</p>
+    <p id="p2victories"></p>`;
 
-containerBox6.innerHTML = `
-    <h1>Ha ganado el Jugador 2</h1>
-`;
 
 /*
 let player1;
@@ -194,43 +214,75 @@ playerBox.value = arrayFighters[0].name;*/
 
 
 let turn = 1;
+let nCombat = 0;
+let idFighting = 0;
+let nGames = 0;
+
+
 function attack(){
-    if(turn == 1){
-        console.log("Player actual:", arrayFighters[0].name);
-        arrayFighters[0].punchAttack(arrayFighters[1]);
-        //localStorage.setItem("textLifeP1", player1.life)
-        textLifeP2 = document.getElementById("textLifeP2");
-        textLifeP2.value = arrayFighters[1].life;
-        if(arrayFighters[1].life < 0) console.log("Player 1 wins");
-        
-        let bar2 = document.querySelector("#barraVida2");
-        if(arrayFighters[1].life < 0) arrayFighters[1].life = 0;
-        bar2Value = (20/500)*arrayFighters[1].life + "em";
-        bar2.style.width =bar2Value;
-        
-        turn = 2;
-    }else{
-        console.log("Player actual:", arrayFighters[1].name);
-        arrayFighters[1].punchAttack(arrayFighters[0]);
-        textLifeP1 = document.getElementById("textLifeP1");
-        textLifeP1.value = arrayFighters[0].life;
-        if(arrayFighters[0].life < 0) console.log("Player 2 wins");
+    if(nGames < nMaxFighters){
+        console.log("Player 1:", arrayPlayers[0]);
+        console.log("Player 2:", arrayPlayers[1]);
+        console.log("ID FIghting", idFighting);
+        if(turn == 1){
+            console.log("Player actual 1:", arrayPlayers[0].getLuchador()[idFighting].name);
+            arrayPlayers[0].getLuchador()[idFighting].punchAttack(arrayPlayers[1].getLuchador()[idFighting]);
+            //localStorage.setItem("textLifeP1", player1.life)
+            textLifeP2 = document.getElementById("textLifeP2");
+            textLifeP2.value = arrayPlayers[1].getLuchador()[idFighting].life;
+            if(arrayPlayers[1].getLuchador()[idFighting].life < 0) console.log("Player 1 wins");
+            
+            let bar2 = document.querySelector("#barraVida2");
+            if(arrayPlayers[1].getLuchador()[idFighting].life < 0) arrayPlayers[1].getLuchador()[idFighting].life = 0;
+            bar2Value = (20/500)*arrayPlayers[0].getLuchador()[idFighting].life + "em";
+            bar2.style.width =bar2Value;
+            
+            turn = 2;
+        }else{
+            console.log("Player actual 2:", arrayPlayers[1].getLuchador()[idFighting].name);
+            arrayPlayers[1].getLuchador()[idFighting].punchAttack(arrayPlayers[0].getLuchador()[idFighting]);
+            textLifeP1 = document.getElementById("textLifeP1");
+            textLifeP1.value = arrayPlayers[0].getLuchador()[idFighting].life;
+            if(arrayPlayers[0].getLuchador()[idFighting].life < 0) console.log("Player 2 wins");
 
-        let bar1 = document.querySelector("#barraVida1");
-        if(arrayFighters[0].life < 0) arrayFighters[0].life = 0;
-        bar1Value = (20/500)*arrayFighters[0].life + "em";
-        bar1.style.width = bar1Value;
+            let bar1 = document.querySelector("#barraVida1");
+            if(arrayPlayers[0].getLuchador()[0].life < 0) arrayPlayers[0].getLuchador()[idFighting].life = 0;
+            bar1Value = (20/500)*arrayPlayers[0].getLuchador()[idFighting].life + "em";
+            bar1.style.width = bar1Value;
 
-        turn = 1;
-    }
+            turn = 1;
+        }
+        if( arrayPlayers[0].getLuchador()[idFighting].life <= 0 ){
+            arrayPlayers[1].aumentaContador();
+            console.log("aumenta id figh");
+            idFighting++;
+            nGames++;
+            console.log("Games:", nGames);
+        }else if(arrayPlayers[1].getLuchador()[idFighting].life <= 0){
+            arrayPlayers[0].aumentaContador();
+            console.log("aumenta id figh");
+            idFighting++;
+            nGames++;
+            console.log("Games:", nGames);
+        }
 
-    if( arrayFighters[0].life <= 0 ){
-        containerBox4.style.display = 'none'; 
-        containerBox6.style.display = '';
-    }else if(arrayFighters[1].life <= 0){
+    } else{
         containerBox4.style.display = 'none'; 
         containerBox5.style.display = '';
+        console.log("FIN DEL JUEGO");
+        let winner = document.querySelector("#winner");
+        let p1vic = document.querySelector("#p1victories");
+        let p2vic = document.querySelector("#p2victories");
+        p1vic.innerHTML = arrayPlayers[0].getContador();
+        p2vic.innerHTML = arrayPlayers[1].getContador();
+        if( arrayPlayers[0].getContador() > arrayPlayers[1].getContador() ){
+            winner.innerHTML=`El jugador 1`;
+        }else{
+            winner.innerHTML=`El jugador 2`;
+        }
+            
+            
     }
-    console.log("Vida Player1",arrayFighters[0].life);
-    console.log("Vida Player2",arrayFighters[1].life);
+    //console.log("Vida Player1",arrayPlayers[0].getLuchador()[idFighting].life);
+    //console.log("Vida Player2",arrayPlayers[1].getLuchador()[idFighting].life);
 }
